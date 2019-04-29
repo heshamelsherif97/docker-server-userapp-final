f2633091a59ac0475a04c40c899d4957d751620b
thetopluca <topluca17@gmail.com>
2019-04-29 01:10:09 +0000

    lastupdate

--- src/main/java/CommandPattern/userStories/AppThreadPool.java
+++ src/main/java/CommandPattern/userStories/AppThreadPool.java
@@ -2,13 +2,33 @@ package CommandPattern.userStories;
 
 import CommandPattern.Controller.PropertiesHandler;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
 public class AppThreadPool {
-    static int numberOfThreads = Integer.parseInt(PropertiesHandler.getProperty("max_app_threads"));
+    static int numberOfThreads = initializeNumberofthreads();//Integer.parseInt(PropertiesHandler.getProperty("max_app_threads"));
     private static ExecutorService executor = Executors.newFixedThreadPool( numberOfThreads);
+
+
+    private static int initializeNumberofthreads(){
+        Properties conf = new Properties();
+        try {
+
+            File file = new File("./src/main/java/CommandPattern/userStories/config.properties");
+            FileInputStream confLoc = new FileInputStream(file);
+            conf.load(confLoc);
+            int number = Integer.parseInt(conf.getProperty("max_app_threads"));
+            return number;
+        } catch (Exception x) {
+            x.printStackTrace();
+            return 1;
+        }
+    }
+
 
     public static ExecutorService getInstance(){
         return executor;
